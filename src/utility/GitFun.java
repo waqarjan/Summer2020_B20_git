090fe6c70207a24ee22a0529c2b9dfec0409ba50
WAQAR JAN <68917842+waqarjan@users.noreply.github.com>
2020-08-05 21:16:34 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,7 +4,8 @@ public class GitFun {
     public static void main(String[] args) {
 
         System.out.println("Everything is Fun");
-
+        
+//how much funn do we have? alot some says....
 
 
     }
